414742b84bf098271e1297e811552ef11e9f8334
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2025-02-03 10:47:04 +0000

    7B model

--- test_2.py
+++ test_2.py
@@ -5,7 +5,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, DynamicCache
 import torch
 
 
-llm_name = "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
+llm_name = "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B"
 temperature = 0.7
 max_length = 10000
 num_responses = 100
