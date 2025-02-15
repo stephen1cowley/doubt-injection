d9ba776043ef5cda323225cab5820046e685d93f
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2025-02-15 16:58:32 +0000

    idea extrction

--- idea_extraction.py
+++ idea_extraction.py
@@ -1,18 +1,19 @@
 from typing import List
 import os
+import time
 import json
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
 IDEAS_LIST = [
-    "the farmer returning alone",
-    "the setup in the classic version being different",
-    "the farmer's presence prevents adverse interactions",
-    "the puzzle being unsolvable",
+    "'the farmer returning alone'",
+    "'the setup in the classic version being different'",
+    "'the farmer's presence prevents adverse interactions'",
+    "'the puzzle is unsolvable'",
     "'I'm confused'",
-    "the wolf and cabbage starting on the left",
-    "the solution seems too simple"
+    "'the wolf and cabbage start on the left'",
+    "'the solution seems too simple'",
     "7 numbered steps to solve the puzzle"
 ]
 
@@ -39,7 +40,7 @@ def make_coherent(
 
 
 # Query LLM
-llm_name = "Qwen/Qwen2.5-1.5B"
+llm_name = "Qwen/Qwen2.5-32B"
 tokenizer = AutoTokenizer.from_pretrained(llm_name)
 model = AutoModelForCausalLM.from_pretrained(llm_name, torch_dtype=torch.bfloat16)
 model = model.to("cuda" if torch.cuda.is_available() else "cpu")  # Move model to GPU if available
@@ -48,40 +49,57 @@ print(f"Model dtype: {model.dtype}")
 print(f"First layer dtype: {next(model.parameters()).dtype}")
 
 
-response_files: List[str] = os.listdir("responses")
+response_files: List[str] = os.listdir("responses")[-250:]
 
 
-# Read last response
-with open("responses/" + response_files[-1], 'r') as f:
-    response: str = json.load(f)[0]
+for response_file in response_files:
+    time_0 = time.time()
+    ideas: List[bool] = [False] * len(IDEAS_LIST)
 
-# Split up into N characters with overlap
-chunk_size = 500
-chunks = [response[i:i + chunk_size] for i in range(0, len(response), chunk_size//2)]
-chunks[0] = make_coherent(chunks[0], remove_start=False)
-chunks[-1] = make_coherent(chunks[-1], remove_end=False)
-for i in range(1, len(chunks)-1):
-    chunks[i] = make_coherent(chunks[i])
+    # Read last response
+    with open("responses/" + response_file, 'r') as f:
+        response: str = json.load(f)[0]
 
+    # Split up into N characters with overlap
+    chunk_size = 500
+    chunks = [response[i:i + chunk_size] for i in range(0, len(response), chunk_size//2)]
+    chunks[0] = make_coherent(chunks[0], remove_start=False)
+    chunks[-1] = make_coherent(chunks[-1], remove_end=False)
+    for i in range(1, len(chunks)-1):
+        chunks[i] = make_coherent(chunks[i])
 
-for idea in IDEAS_LIST:
-    for i in range(len(chunks)):
-        print("\n\n--------------------------------------")
+    for idea_id in range(len(IDEAS_LIST)):
+        for i in range(len(chunks)):
+            if chunks[i] == "":
+                continue
 
-        prompt = f"""
+            print("\n\n--------------------------------------")
+
+            prompt = f"""
 Given the following passage, give an answer which is either 'true' or 'false'.
 Passage:
 
 {chunks[i]}
 
 (end of passage)
-Did the passage contain mention of {idea}?
+Did the passage contain mention of {IDEAS_LIST[idea_id]}?
 Answer (true/false): """
 
-        input_ids = tokenizer.encode(prompt, return_tensors="pt").to(model.device)
-        output = model.generate(
-            input_ids,
-            max_new_tokens=1
-        )
+            input_ids = tokenizer.encode(prompt, return_tensors="pt").to(model.device)
+            output = model.generate(
+                input_ids,
+                max_new_tokens=1
+            )
 
-        print(tokenizer.decode(output[0], skip_special_tokens=True))
+            print(tokenizer.decode(output[0], skip_special_tokens=True))
+            ans = tokenizer.decode(output[0], skip_special_tokens=True)[len(prompt):]
+            print("\n ANS:", ans)
+
+            if "true" in ans:
+                ideas[idea_id] = True
+                break
+
+    with open(f"ideas/ideas_{response_file}.json", "w") as f:
+        json.dump(ideas, f)
+
+    print(f"Reponse took {time.time() - time_0}")
