b0d11275d2711f6ce6a6b76ee0be919de3fefa81
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2025-02-16 21:53:19 +0000

    analyse json files

---
+++ idea_analysis.py
@@ -0,0 +1,17 @@
+from typing import List
+import json
+import os
+
+idea_files: List[str] = os.listdir("ideas")
+results: List[List[int]] = [[0 for _ in range(10)] for _ in range(5)]
+
+for i in range(5):
+    for j in range(50):
+        # Read last response
+        with open("ideas/" + idea_files[i*50+j], 'r') as f:
+            ideas: List[bool] = json.load(f)
+
+        for k in range(len(ideas)):
+            results[i][k] += int(ideas[k])
+
+print(results)
