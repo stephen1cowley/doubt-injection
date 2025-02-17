3be0aedb30d5e091ddbe6113c04533acb499e6e8
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2025-02-17 16:49:49 +0000

    verbosity

--- idea_analysis.py
+++ idea_analysis.py
@@ -4,9 +4,11 @@ import os
 
 idea_files: List[str] = sorted(os.listdir("ideas"))
 print(len(idea_files))
+
 results: List[List[int]] = [[0 for _ in range(10)] for _ in range(5)]
 
 for i in range(5):
+    print(idea_files[i*50], idea_files[i*50+49])
     for j in range(50):
         # Read last response
         with open("ideas/" + idea_files[i*50+j], 'r') as f:
