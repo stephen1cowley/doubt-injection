5010ac5fa4edff383e02193e5b5160c5356bfe23
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2025-03-01 17:40:32 +0000

    keep dataclass file separate

--- simplebench_eval.py
+++ simplebench_eval.py
@@ -4,20 +4,8 @@ import torch
 import time
 import argparse
 from transformers import AutoTokenizer, AutoModelForCausalLM, DynamicCache
-from dataclasses import dataclass, asdict
-
-
-@dataclass
-class ExperimentResult:
-    temperature: float
-    response_length: int
-    llm_answer: str
-    correct_answer: str
-    llm_name: str
-    question_id: int
-    top_p: float
-    prompt_name: str
-    doubt_injection_prob: float
+from dataclasses import asdict
+from experiment_types import ExperimentResult
 
 
 def parse_args():
