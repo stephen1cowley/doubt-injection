c7893f2fd065a1ec5ea64adc14f50b992bc0c359
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2025-03-15 11:29:16 +0000

    gaps to fill again

--- sbatch/run_diff_prompts_sb_20min_redo.sh
+++ sbatch/run_diff_prompts_sb_20min_redo.sh
@@ -25,17 +25,17 @@ injection_strings=(
 # sbatch simplebench_eval_20mins.wilkes3 9 25 1.0 "\"But wait, I should think about the real world.\""
 # sbatch simplebench_eval_20mins.wilkes3 10 25 1.0 "\"But wait, I should think about the real world.\""
 
-for i in 1 2 3 8 9 10; do
-    for _ in {1..20}; do
-        sbatch simplebench_eval_20mins.wilkes3 $i 25 1.0 "But"
-    done
-done
+# for i in 1 2 3 8 9 10; do
+#     for _ in {1..20}; do
+#         sbatch simplebench_eval_20mins.wilkes3 $i 25 1.0 "But"
+#     done
+# done
 
-for i in 1 2 3 8 9 10; do
-    for _ in {1..30}; do
-        sbatch simplebench_eval_20mins.wilkes3 $i 25 1.0 "\"But wait, let me think again.\""
-    done
-done
+# for i in 1 2 3 8 9 10; do
+#     for _ in {1..30}; do
+#         sbatch simplebench_eval_20mins.wilkes3 $i 25 1.0 "\"But wait, let me think again.\""
+#     done
+# done
 
 # sbatch simplebench_eval_20mins.wilkes3 9 25 1.0 "\"But wait, let me think again.\""
 # sbatch simplebench_eval_20mins.wilkes3 9 25 1.0 "\"But wait, let me think again.\""
@@ -43,3 +43,11 @@ done
 # sbatch simplebench_eval_20mins.wilkes3 9 25 1.0 "\"But wait, let me think again.\""
 # sbatch simplebench_eval_20mins.wilkes3 9 25 1.0 "\"But wait, let me think again.\""
 # sbatch simplebench_eval_20mins.wilkes3 10 25 1.0 "\"But wait, let me think again.\""
+
+sbatch simplebench_eval_20mins.wilkes3 9 25 1.0 "But"
+sbatch simplebench_eval_20mins.wilkes3 9 25 1.0 "But"
+sbatch simplebench_eval_20mins.wilkes3 1 25 1.0 "\"But wait, let me think again.\""
+sbatch simplebench_eval_20mins.wilkes3 8 25 1.0 "\"But wait, let me think again.\""
+sbatch simplebench_eval_20mins.wilkes3 9 25 1.0 "\"But wait, let me think again.\""
+sbatch simplebench_eval_20mins.wilkes3 10 25 1.0 "\"But wait, let me think again.\""
+
