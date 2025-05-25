9f150b7af44df0132842eaa0cb2ec85d909cd41f
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2025-05-25 15:55:12 +0100

    9800

--- results_doubtprob_analysis.py
+++ results_doubtprob_analysis.py
@@ -70,7 +70,7 @@ for file in files:
             #     )
             
             # Determine if length of response is greater than 10000
-            if result["response_length"] > 10000:
+            if result["response_length"] > 9800:
                 results_per_doubtprob[question_id][temperature][doubt_injection_prob] = (
                     results_per_doubtprob[question_id][temperature][doubt_injection_prob][0] + 1,
                     results_per_doubtprob[question_id][temperature][doubt_injection_prob][1] + 1
